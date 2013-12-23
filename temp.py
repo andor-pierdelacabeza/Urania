29e4249bb286d1fcd69100322727004d426f0f89
Alvaro [Andor] <andor@pierdelacabeza.com>
2013-12-23 20:24:02 +0000

    added platform options for temp fetching

--- temp.py
+++ temp.py
@@ -1,15 +1,22 @@
 import os
 import subprocess
+from tornado.options import define, options
+import opts
 
 def getCpuTemp():
-	cpufile = open('/sys/class/thermal/thermal_zone0/temp')
-	cputemp = cpufile.read(2) + '.' + cpufile.read(1)
-	cpufile.close()
-	return(cputemp)
-
+	if options.platform == 'rpi':
+		cpufile = open('/sys/class/thermal/thermal_zone0/temp')
+		cputemp = cpufile.read(2) + '.' + cpufile.read(1)
+		cpufile.close()
+		return(cputemp)
+	else:
+		return(00)
 
 def getGpuTemp():
-	gputemp = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
-	gputemp = gputemp[5:9]
-	return(gputemp)
+	if options.platform == 'rpi':
+		gputemp = subprocess.check_output(["/opt/vc/bin/vcgencmd", "measure_temp"])
+		gputemp = gputemp[5:9]
+		return(gputemp)
+	else:
+		return(00)
 
